b392085c9e4334e0db332b29cf739d676803e1f3
LocalMachine <157286208+MaquinaLocal@users.noreply.github.com>
2024-04-25 23:23:51 -0300

    Movimiento basado en clicks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,10 +15,14 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Transform fishSprite;
     [SerializeField] private PlayerUI playerUI;
+    [SerializeField] private Rigidbody2D rb;
 
     
     void Start()
     {
+        // Inicialización de RigidBody
+        rb = GetComponent<Rigidbody2D>();
+
         // Obtener los límites de pantalla
         screenLimit_x = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
         screenLimit_y = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y;
@@ -30,26 +34,15 @@ public class Player : MonoBehaviour
   
     void Update()
     {
-        // Desplazamiento
-        float inputRight = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float inputUp = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        MovePlayerToLocation();
 
-        Vector3 currentPosition = transform.position + new Vector3(inputRight, inputUp, 0);
+        Vector3 currentPosition = transform.position;
 
         // Establecer el limite min y max de desplazamiento
         currentPosition.x = Mathf.Clamp(currentPosition.x, -screenLimit_x, screenLimit_x);
         currentPosition.y = Mathf.Clamp(currentPosition.y, -screenLimit_y, screenLimit_y);
 
         transform.position = currentPosition;
-
-        // Rotación
-        if (inputRight == 0) return;
-        if (inputRight < 0)
-            fishSprite.rotation = Quaternion.Euler(0, 180, 0);
-        else
-            fishSprite.rotation = Quaternion.Euler(0, 0, 0);
-        
-        //Debug.Log(transform.position.x + " - " + Screen.width);
     }
 
 
@@ -83,5 +76,30 @@ public class Player : MonoBehaviour
                 GameManager.Instance.UpdateStates(States.GameOver);
             }
         }
+    }
+
+
+    // Movimiento de Jugador por Mouse
+    private void MovePlayerToLocation()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // Click a posición en pantalla
+            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.z = 0f;
+
+            // Diferencia entre jugador y lugar del click
+            Vector2 moveDirection = (targetPosition - transform.position).normalized;
+
+            // Rotación
+            if (moveDirection.x < 0) 
+                fishSprite.rotation = Quaternion.Euler(0, 180, 0);
+            else 
+                fishSprite.rotation = Quaternion.Euler(0, 0, 0);
+
+            // Movimiento usando Rigidbody
+            rb.MovePosition(rb.position + moveDirection * speed * Time.deltaTime);
+           
+        }        
     }
 }
